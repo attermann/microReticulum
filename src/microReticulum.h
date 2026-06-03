6b23ee7820809004474da20780e38efebfd91089
Chad Attermann <attermann@gmail.com>
2026-06-03 11:47:36 -0600

    Added master header file to replace individual header requirement

---
+++ src/microReticulum.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026 Chad Attermann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+#pragma once
+
+#include "Utilities/OS.h"
+#include "Provisioning/Provisioning.h"
+#include "Transport.h"
+#include "Reticulum.h"
+#include "Link.h"
+#include "Resource.h"
+#include "Interface.h"
+#include "Packet.h"
+#include "Destination.h"
+#include "Identity.h"
+#include "Bytes.h"
+#include "Log.h"
+#include "Type.h"
